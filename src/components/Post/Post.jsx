e9bcc20b32f21370dab9a21841d3e7c4da1f1686
OriontDev <madeoriontfedora@gmail.com>
2025-08-26 14:13:35 +0800

    Post footer

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,6 +1,7 @@
 import styles from './Post.module.css';
 import pfp from '/public/pfp.png'; //loading pfp
 import { useNavigate } from 'react-router-dom';
+import exitlogo from '/exitlogo.svg'
 
 function Post(){
 
@@ -15,7 +16,19 @@ function Post(){
                         <p className={styles.username}>@aaa</p>
                         <p className={styles.date}>August 1th 1943</p>
                     </div>
+                    <div className={styles.postinfobutton}>•••</div>
+                    <div className={styles.deletepostbutton}>X</div>
                 </div>
+                <div className={styles.messagecontainer}>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero neque delectus tenetur sapiente veniam optio rem quas similique impedit ratione exercitationem at, nisi, ad dolores quaerat eos deleniti voluptate rerum?</p>
+                </div>
+                <div className={styles.footercontainer}>
+                    <div className={styles.likecontainer}>
+                        <div className={styles.likelogo}></div>
+                        <p>aaaaa</p>
+                    </div>
+                </div>
+
             </div>
         </>
     );
